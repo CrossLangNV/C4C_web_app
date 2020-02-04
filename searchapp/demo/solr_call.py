e2792e9c440f540974e4630c7be160aaae14418b
frederic.everaert <frederic.everaert@crosslang.com>
2020-02-04 09:54:01 +0100

    added highlighting

--- searchapp/demo/solr_call.py
+++ searchapp/demo/solr_call.py
@@ -1,16 +1,23 @@
 import pysolr
 
+
 def solr_search(name="", cat=""):
     client = pysolr.Solr('http://localhost:8983/solr/demo')
-    search = get_results(client.search('name:' + name, **{'rows': 100, 'hl': 'true'}))
+    search = get_results(client.search('name:' + name,
+                                       **{'rows': 100, 'hl': 'on', 'hl.fl': 'name',
+                                          'hl.simple.pre': '<span class="highlight">',
+                                          'hl.simple.post': '</span>'}))
     return search
+
 
 def get_results(response):
     # parse
     results = []
     for result in response:
+        result['name'] = response.highlighting[result['id']]['name']
         results.append(result)
     return results
 
+
 if __name__ == '__main__':
-    print("Search demo | name = game:\n", solr_search(name = "game"))
+    print("Search demo | name = game:\n", solr_search(name="game"))
