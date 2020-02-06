cb94c11126ec09d7bb9dda365f592aa930b41ffb
frederic.everaert <frederic.everaert@crosslang.com>
2020-02-06 15:57:53 +0100

    angular and bootstrap: sortable table + pagination

--- angular/angular-app/src/app/app.module.ts
+++ angular/angular-app/src/app/app.module.ts
@@ -1,19 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FilmListComponent } from './film-list/film-list.component';
+import { FilmListComponent, NgbdSortableHeaderDirective } from './film-list/film-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FilmListComponent
+    FilmListComponent,
+    NgbdSortableHeaderDirective,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NgbModule,
+    FormsModule,
     HttpClientModule
   ],
   providers: [],
--- angular/angular-app/src/app/film-list/film-list.component.ts
+++ angular/angular-app/src/app/film-list/film-list.component.ts
@@ -1,7 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Directive, EventEmitter, Input, Output, QueryList, ViewChildren } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Film } from '../film';
 import { ApiService } from '../api.service';
+
+export type SortDirection = 'asc' | 'desc' | '';
+const rotate: {[key: string]: SortDirection} = { 'asc': 'desc', 'desc': '', '': 'asc' };
+export const compare = (v1, v2) => v1 < v2 ? -1 : v1 > v2 ? 1 : 0;
+
+export interface SortEvent {
+  column: string;
+  direction: SortDirection;
+}
+
+@Directive({
+  selector: 'th[sortable]',
+  host: {
+    '[class.asc]': 'direction === "asc"',
+    '[class.desc]': 'direction === "desc"',
+    '(click)': 'rotate()'
+  }
+})
+
+export class NgbdSortableHeaderDirective {
+
+  @Input() sortable: string;
+  @Input() direction: SortDirection = '';
+  @Output() sort = new EventEmitter<SortEvent>();
+
+  rotate() {
+    this.direction = rotate[this.direction];
+    this.sort.emit({column: this.sortable, direction: this.direction});
+  }
+}
 
 @Component({
   selector: 'app-film-list',
@@ -9,16 +40,52 @@ import { ApiService } from '../api.service';
   styleUrls: ['./film-list.component.css']
 })
 export class FilmListComponent implements OnInit {
-  films$: Observable<Film[]>;
+
+  @ViewChildren(NgbdSortableHeaderDirective) headers: QueryList<NgbdSortableHeaderDirective>;
+
+  page = 1;
+  pageSize = 50;
+  cachedFilms = [];
+  collectionSize = 0;
 
   constructor(private apiService: ApiService) { }
 
   ngOnInit() {
-    this.getFilms();
+    this.apiService.getFilms().subscribe(films => {
+      this.cachedFilms = films as Film[];
+      this.collectionSize = this.cachedFilms.length;
+    });
   }
 
-  public getFilms() {
-    this.films$ = this.apiService.getFilms();
+  get films(): Film[] {
+    return this.cachedFilms.
+      slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
+  }
+
+  set films(films: Film[]) {
+    this.films = films;
+  }
+
+  onSort({column, direction}: SortEvent) {
+
+    // resetting other headers
+    this.headers.forEach(header => {
+      if (header.sortable !== column) {
+        header.direction = '';
+      }
+    });
+
+    // sorting films
+    if (direction === '') {
+      this.apiService.getFilms().subscribe(films => {
+        this.cachedFilms = films as Film[];
+      });
+    } else {
+      this.cachedFilms = this.cachedFilms.sort((a, b) => {
+        const res = compare(a[column], b[column]);
+        return direction === 'asc' ? res : -res;
+      });
+    }
   }
 
 }
