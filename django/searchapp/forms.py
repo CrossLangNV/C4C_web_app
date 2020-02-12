7875ce1db9426a24e026873563531305f98094c3
frederic.everaert <frederic.everaert@crosslang.com>
2020-02-12 11:08:31 +0100

    django: document date picker

--- django/searchapp/forms.py
+++ django/searchapp/forms.py
@@ -1,12 +1,16 @@
 from django import forms
 from .models import AcceptanceState
 
+
 class CreateDocument(forms.Form):
     title = forms.CharField(max_length=150)
-    date = forms.DateField()
+    date = forms.DateField(widget=forms.TextInput(
+        attrs={'type': 'date'}
+    ))
     acceptance_state = forms.ChoiceField(choices=AcceptanceState.choices)
     url = forms.CharField(max_length=150)
     content = forms.CharField(widget=forms.Textarea)
+
 
 class CreateWebsite(forms.Form):
     name = forms.CharField(max_length=32)
