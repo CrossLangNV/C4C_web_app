04742ac4be443900679f88ccda0c464cc8af2e21
frederic.everaert <frederic.everaert@crosslang.com>
2020-02-14 19:07:14 +0100

    django: model fields updated, form datepicker widget

--- django/searchapp/forms.py
+++ django/searchapp/forms.py
@@ -6,6 +6,11 @@ class DocumentForm(forms.ModelForm):
     class Meta:
         model = Document
         fields = ['title', 'date', 'acceptance_state', 'url', 'content']
+        widgets = {
+            'date': forms.DateInput(attrs={
+                'type': 'date'
+            })
+        }
 
 
 class WebsiteForm(forms.ModelForm):
--- django/searchapp/models.py
+++ django/searchapp/models.py
@@ -3,9 +3,9 @@ import uuid
 from .solr_call import solr_add
 
 class Website(models.Model):
-    name = models.CharField(max_length=32)
+    name = models.CharField(max_length=200)
     content = models.TextField()
-    url = models.CharField(max_length=100)
+    url = models.URLField()
 
     def __str__(self):
         return self.name
@@ -18,12 +18,12 @@ class AcceptanceState(models.TextChoices):
 class Document(models.Model):
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(max_length=150)
+    title = models.CharField(max_length=500)
     date = models.DateField()
     acceptance_state = models.CharField(max_length=20,
                                  choices=AcceptanceState.choices,
                                  default=AcceptanceState.UNVALIDATED)
-    url = models.CharField(max_length=150)
+    url = models.URLField()
     website = models.ForeignKey('Website', on_delete=models.CASCADE)
     content = models.TextField(default="")
 
