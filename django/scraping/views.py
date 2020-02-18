1c3e5d9d23b26fc586dee5b818b98559340a86fe
frederic.everaert <frederic.everaert@crosslang.com>
2020-02-18 16:14:13 +0100

    django: added scraping app, using scrapy and scrapyd, post route to start scraping job with given spider, get route to retrieve scraped result from db

---
+++ django/scraping/views.py
@@ -0,0 +1,51 @@
+import os
+from uuid import uuid4
+
+from django.http import JsonResponse
+from django.utils.decorators import method_decorator
+from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic.detail import ContextMixin, TemplateResponseMixin
+from scrapyd_api import ScrapydAPI
+
+from .models import ScrapyItem
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class ScrapingTemplateView(View, ContextMixin, TemplateResponseMixin):
+    template_name = "scraping/scraping.html"
+    scrapyd = ScrapydAPI(os.environ['SCRAPYD_URL'])
+
+    # query db for scraped item
+    def get(self, request):
+        unique_id = request.GET.get('unique_id', None)
+
+        if not unique_id:
+            return JsonResponse({'error': 'Missing unique_id'})
+
+        try:
+            # this is the unique_id that we created even before crawling started.
+            item = ScrapyItem.objects.get(unique_id=unique_id)
+            return JsonResponse({'data': item.to_dict['data']})
+        except Exception as e:
+            return JsonResponse({'error': str(e)})
+
+    # new scraping task
+    def post(self, request):
+        spider = request.POST.get('spider', None)
+
+        if not spider:
+            return JsonResponse({'error': 'Missing spider'})
+
+        unique_id = str(uuid4())  # create a unique ID
+
+        # custom settings for spider
+        settings = {
+            'unique_id': unique_id,
+            'USER_AGENT': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
+        }
+
+        # schedule scraping task
+        task = self.scrapyd.schedule('default', spider, settings=settings)
+
+        return JsonResponse({'task_id': task, 'unique_id': unique_id, 'status': 'started'})
