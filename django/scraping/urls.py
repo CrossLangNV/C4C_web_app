390814de04f37c7e6d19ee61569d11a96406ab48
frederic.everaert <frederic.everaert@crosslang.com>
2020-02-18 17:07:41 +0100

    django: scraping view

--- django/scraping/urls.py
+++ django/scraping/urls.py
@@ -1,7 +1,8 @@
-from django.contrib.auth.decorators import login_required
 from django.urls import path
+
 from scraping import views
 
 urlpatterns = [
     path('', views.ScrapingTemplateView.as_view(), name='scraping'),
+    path('<spider>', views.ScrapingTemplateView.as_view(), name='spider'),
 ]
