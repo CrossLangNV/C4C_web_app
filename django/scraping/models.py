d99a990e7fa8eff79a929d3b71969b52bcd338e1
frederic.everaert <frederic.everaert@crosslang.com>
2020-02-20 16:35:47 +0100

    django: spider field for ScrapyItem

--- django/scraping/models.py
+++ django/scraping/models.py
@@ -4,8 +4,14 @@ from django.utils import timezone
 
 class ScrapyItem(models.Model):
     unique_id = models.CharField(max_length=100, null=True)
+    spider = models.CharField(max_length=100, default="")
     data = models.TextField()  # crawled data
     date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.unique_id
+
+    def save(self, *args, **kwargs):
+        # TODO: postprocess
+
+        super().save(*args, **kwargs)
--- django/scrapy_app/scrapy_app/pipelines.py
+++ django/scrapy_app/scrapy_app/pipelines.py
@@ -5,21 +5,25 @@ import requests
 
 
 class ScrapyAppPipeline(object):
-    def __init__(self, unique_id, *args, **kwargs):
+    def __init__(self, unique_id, spider, *args, **kwargs):
         self.unique_id = unique_id
+        self.spider = spider
         self.items = []
         self.django_api_url = os.environ['DJANGO_SCRAPING_API_URL']
 
     @classmethod
     def from_crawler(cls, crawler):
         return cls(
-            unique_id=crawler.settings.get('unique_id'),  # this will be passed from django view
+            # this will be passed from django view
+            unique_id=crawler.settings.get('unique_id'),
+            spider=crawler.settings.get('spider')
         )
 
     def close_spider(self, spider):
         # save crawled data to django through API call
         item = {}
         item['unique_id'] = self.unique_id
+        item['spider'] = self.spider
         item['data'] = json.dumps(self.items)
         requests.post(self.django_api_url + '/task/', json=item)
 
