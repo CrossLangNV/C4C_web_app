5bb5d7fb81c544a3217978edd4f31b209e5cbcdc
frederic.everaert <frederic.everaert@crosslang.com>
2020-02-25 09:42:14 +0100

    django: fixed EiopaItemHandler Document update, other minor changes

--- django/scraping/urls.py
+++ django/scraping/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
 
     path('api/task/', views.ScrapingTaskListView.as_view(), name='scraping-task-list'),
     path('api/task/<int:pk>/', views.ScrapingTaskView.as_view(), name='scraping-task'),
-    path('api/task/<int:pk>/postprocess/', views.PostprocessScrapyItem.as_view(), name='postprocess'),
+    path('api/task/<int:pk>/postprocess/', views.PostprocessScrapingItem.as_view(), name='postprocess'),
 ]
