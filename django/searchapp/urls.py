2271e32d95524131b1f758bdb82fb3eef56f4ddd
frederic.everaert <frederic.everaert@crosslang.com>
2020-02-27 12:19:51 +0100

    django: SolrFile search API

--- django/searchapp/urls.py
+++ django/searchapp/urls.py
@@ -56,4 +56,5 @@ urlpatterns = [
     path('api/attachment/', views.AttachmentListAPIView.as_view(), name='attachment_list_api'),
     path('api/attachment/<uuid:pk>/', views.AttachmentDetailAPIView.as_view(), name='attachment_detail_api'),
     path('api/solrfiles/', views.SolrFileList.as_view(), name='solr_file_list_api'),
+    path('api/solrfiles/<search_term>', views.SolrFile.as_view(), name='solr_file_search_api'),
 ]
