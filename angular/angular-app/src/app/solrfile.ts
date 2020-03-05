b7cdd9e06bcec7674eb6eb892d0f0a51df1c49ff
frederic.everaert <frederic.everaert@crosslang.com>
2020-03-05 15:16:14 +0100

    angular: api/solrfiles model-adapter

--- angular/angular-app/src/app/solrfile.ts
+++ angular/angular-app/src/app/solrfile.ts
@@ -1,6 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Adapter } from './adapter';
+
 export class SolrFile {
-  id: string;
-  language: string;
-  doc_type: string;
-  content: string;
+  constructor(
+    public id: string,
+    public documentId: string,
+    public url: string,
+    public date: Date,
+    public language: string,
+    public docType: string,
+    public content: string
+  ) {}
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SolrFileAdapter implements Adapter<SolrFile> {
+  adapt(item: any): SolrFile {
+    return new SolrFile(
+      item.id,
+      item.attr_document_id[0],
+      item.attr_url[0],
+      new Date(item.attr_date[0]),
+      item.language,
+      item.doc_type,
+      item.content
+    );
+  }
 }
