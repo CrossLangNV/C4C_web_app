343656718faa340bfe975713c794b27b761e12ec
frederic.everaert <frederic.everaert@crosslang.com>
2020-03-11 13:39:36 +0100

    django: scraping api detail view, available spiders

--- django/scraping/urls.py
+++ django/scraping/urls.py
@@ -8,6 +8,5 @@ urlpatterns = [
 
     path('api/task/', views.ScrapingTaskListView.as_view(), name='scraping-task-list'),
     path('api/task/<int:pk>/', views.ScrapingTaskView.as_view(), name='scraping-task'),
-    path('api/task/<int:pk>/postprocess/', views.PostprocessScrapingTask.as_view(), name='postprocess-task'),
-    path('api/item/<int:pk>/postprocess/', views.PostprocessScrapingItem.as_view(), name='postprocess-item'),
+    path('api/spider/', views.SpiderListView.as_view(), name='spider-list'),
 ]
