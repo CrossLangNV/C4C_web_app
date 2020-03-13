8a47f3a35a5611c7ea719c4d90e5a52f737a2e3d
frederic.everaert <frederic.everaert@crosslang.com>
2020-03-13 14:44:41 +0100

    angular: separate browse routing module + route hierarchy

--- angular/angular-app/src/app/app-routing.module.ts
+++ angular/angular-app/src/app/app-routing.module.ts
@@ -1,20 +1,19 @@
 import { Routes, RouterModule } from '@angular/router';
-import { WebsiteListComponent } from './browse/website-list/website-list.component';
-import { WebsiteDetailsComponent } from './browse/website-details/website-details.component';
 
 import { SolrFileListComponent } from './search/solrfile-list/solrfile-list.component';
 import { LoginComponent } from './shared/login-component/login.component';
 import { RegisterComponent } from './shared/register-component/register.component';
 import { AuthGuard } from './core/guards/auth.guard';
-import { DocumentDetailsComponent } from './browse/document-details/document-details.component';
 
 const routes: Routes = [
   { path: '', component: SolrFileListComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'website', component: WebsiteListComponent },
-  { path: 'website/:websiteId', component: WebsiteDetailsComponent },
-  { path: 'document/:documentId', component: DocumentDetailsComponent },
+  {
+    path: 'browse',
+    loadChildren: () =>
+      import('./browse/browse.module').then(m => m.BrowseModule)
+  },
 
   // otherwise redirect to home
   { path: '**', redirectTo: '' }
--- angular/angular-app/src/app/browse/browse.module.ts
+++ angular/angular-app/src/app/browse/browse.module.ts
@@ -3,9 +3,10 @@ import { SharedModule } from '../shared/shared.module';
 import { WebsiteListComponent } from './website-list/website-list.component';
 import { WebsiteDetailsComponent } from './website-details/website-details.component';
 import { DocumentDetailsComponent } from './document-details/document-details.component';
+import { BrowseRoutingModule } from './browse-routing.module';
 
 @NgModule({
   declarations: [WebsiteListComponent, WebsiteDetailsComponent, DocumentDetailsComponent],
-  imports: [SharedModule]
+  imports: [SharedModule, BrowseRoutingModule]
 })
 export class BrowseModule {}
