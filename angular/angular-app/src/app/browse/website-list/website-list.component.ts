478ae5a84cd4a2ea9d2daddbf5a0c32451e6663a
frederic.everaert <frederic.everaert@crosslang.com>
2020-03-13 12:16:45 +0100

    angular: simplified website-list component

--- angular/angular-app/src/app/browse/website-list/website-list.component.ts
+++ angular/angular-app/src/app/browse/website-list/website-list.component.ts
@@ -1,60 +1,6 @@
-import {
-  Component,
-  OnInit,
-  Directive,
-  EventEmitter,
-  Input,
-  Output,
-  QueryList,
-  ViewChildren
-} from '@angular/core';
-import { Subject } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
 import { ApiServiceWebsites } from '../../core/services/api.service.websites';
 import { Website } from '../../shared/models/website';
-
-export type SortDirection = 'asc' | 'desc' | '';
-const rotate: { [key: string]: SortDirection } = {
-  asc: 'desc',
-  desc: '',
-  '': 'asc'
-};
-
-export const compare = (v1, v2) => {
-  if (v1 === v2) {
-    return 0;
-  } else if (v1 === null || v1 === undefined) {
-    return 1;
-  } else if (v2 === null || v2 === undefined) {
-    return -1;
-  } else {
-    return v1 < v2 ? -1 : 1;
-  }
-};
-
-export interface SortEvent {
-  column: string;
-  direction: SortDirection;
-}
-
-@Directive({
-  selector: 'th[sortable]',
-  host: {
-    '[class.asc]': 'direction === "asc"',
-    '[class.desc]': 'direction === "desc"',
-    '(click)': 'rotate()'
-  }
-})
-
-export class WebsiteNgbdSortableHeaderDirective {
-  @Input() sortable: string;
-  @Input() direction: SortDirection = '';
-  @Output() sort = new EventEmitter<SortEvent>();
-
-  rotate() {
-    this.direction = rotate[this.direction];
-    this.sort.emit({ column: this.sortable, direction: this.direction });
-  }
-}
 
 @Component({
   selector: 'app-website-list',
@@ -62,61 +8,17 @@ export class WebsiteNgbdSortableHeaderDirective {
   styleUrls: ['./website-list.component.css']
 })
 export class WebsiteListComponent implements OnInit {
-  @ViewChildren(WebsiteNgbdSortableHeaderDirective) headers: QueryList<
-    WebsiteNgbdSortableHeaderDirective
-  >;
-
-  page = 1;
-  pageSize = 10;
-  cachedWebsitesBeforeSort = [];
-  cachedWebsites = [];
-  searchTerm = '';
-  searchTermChanged: Subject<string> = new Subject<string>();
-  collectionSize = 0;
+  websites = [];
 
   constructor(private apiServiceWebsites: ApiServiceWebsites) {}
 
   ngOnInit() {
     this.apiServiceWebsites.getWebsites().subscribe(websites => {
-      this.cachedWebsitesBeforeSort = websites as Website[];
-      this.cachedWebsites = [...this.cachedWebsitesBeforeSort];
-      this.collectionSize = this.cachedWebsites.length;
+      this.websites = websites as Website[];
     });
-  }
-
-  get websites(): Website[] {
-    return this.cachedWebsites.slice(
-      (this.page - 1) * this.pageSize,
-      (this.page - 1) * this.pageSize + this.pageSize
-    );
-  }
-
-  set websites(websites: Website[]) {
-    this.websites = websites;
-  }
-
-  onSort({ column, direction }: SortEvent) {
-    // resetting other headers
-    this.headers.forEach(header => {
-      if (header.sortable !== column) {
-        header.direction = '';
-      }
-    });
-
-    // sorting websites
-    if (direction === '') {
-      this.cachedWebsites = [...this.cachedWebsitesBeforeSort];
-    } else {
-      this.cachedWebsites = this.cachedWebsites.sort((a, b) => {
-        const res = compare(a[column], b[column]);
-        return direction === 'asc' ? res : -res;
-      });
-    }
   }
 
   onDelete(id) {
-    this.apiServiceWebsites.deleteWebsite(id).subscribe(
-    );
+    this.apiServiceWebsites.deleteWebsite(id).subscribe();
   }
-
 }
