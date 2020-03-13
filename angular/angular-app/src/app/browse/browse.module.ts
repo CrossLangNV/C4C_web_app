8cf3494e5fa071c1de84ec549e8d0375226522cd
frederic.everaert <frederic.everaert@crosslang.com>
2020-03-13 13:53:34 +0100

    angular: document-details component start

--- angular/angular-app/src/app/browse/browse.module.ts
+++ angular/angular-app/src/app/browse/browse.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { WebsiteListComponent } from './website-list/website-list.component';
 import { WebsiteDetailsComponent } from './website-details/website-details.component';
+import { DocumentDetailsComponent } from './document-details/document-details.component';
 
 @NgModule({
-  declarations: [WebsiteListComponent, WebsiteDetailsComponent],
+  declarations: [WebsiteListComponent, WebsiteDetailsComponent, DocumentDetailsComponent],
   imports: [SharedModule]
 })
 export class BrowseModule {}
