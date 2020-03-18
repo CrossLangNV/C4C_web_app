06b163931e93b0cb48cdb03b01c603aa802025b9
anna.bardadym <anna.bardadym@crosslang.com>
2020-03-18 14:20:31 +0100

    layout slight improvements

--- angular/angular-app/src/app/core/services/api.service.ts
+++ angular/angular-app/src/app/core/services/api.service.ts
@@ -41,19 +41,19 @@ export class ApiService {
   }
 
   public getWebsites(): Observable<Website[]> {
-    // return of([
-    //   new Website("1", "name", "htp://url", "bla", []),
-    // ]);
-    return this.http.get<Website[]>(`${this.API_URL}/website`);
+    return of([
+      new Website("1", "name", "htp://url", "bla", []),
+    ]);
+    // return this.http.get<Website[]>(`${this.API_URL}/website`);
   }
 
   public getWebsite(id: string): Observable<Website> {
-    // return of(
-    //   new Website("1", "name", "htp://url", "bla", []),
-    // );
-    return this.http
-      .get<Website>(`${this.API_URL}/website/${id}`)
-      .pipe(map(item => this.websiteAdapter.adapt(item)));
+    return of(
+      new Website("1", "name", "htp://url", "bla", []),
+    );
+    // return this.http
+    //   .get<Website>(`${this.API_URL}/website/${id}`)
+    //   .pipe(map(item => this.websiteAdapter.adapt(item)));
   }
 
   public createWebsite(website: Website): Observable<any> {
