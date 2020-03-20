e76b716a8ccb0c81fe7ceb6b7f1b7fa04f209993
frederic.everaert <frederic.everaert@crosslang.com>
2020-03-20 15:39:03 +0100

    django: acceptance state per user

--- django/searchapp/admin.py
+++ django/searchapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Website, Attachment, Document
+from .models import Website, Attachment, Document, AcceptanceState
 
 admin.site.register(Website)
 admin.site.register(Document)
 admin.site.register(Attachment)
+admin.site.register(AcceptanceState)
--- django/searchapp/serializers.py
+++ django/searchapp/serializers.py
@@ -1,6 +1,7 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
-from searchapp.models import Attachment, Document, Website
+from searchapp.models import Attachment, Document, Website, AcceptanceState
 
 
 class WebsiteSerializer(serializers.ModelSerializer):
@@ -14,10 +15,36 @@ class WebsiteSerializer(serializers.ModelSerializer):
 class DocumentSerializer(serializers.ModelSerializer):
     website = serializers.PrimaryKeyRelatedField(queryset=Website.objects.all())
     attachments = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    acceptance_state = serializers.SerializerMethodField()
+
+    def get_acceptance_state(self, document):
+        user = self.context['request'].user
+        qs = AcceptanceState.objects.filter(document=document, user=user)
+        serializer = AcceptanceStateSerializer(instance=qs, many=True)
+        state_id = None
+        if len(serializer.data) > 0:
+            state_id = serializer.data[0]['id']
+        else:
+            new_unvalidated_state = AcceptanceState.objects.create(
+                document=document,
+                user=user
+            )
+            state_id = new_unvalidated_state.id
+        return state_id
 
     class Meta:
         model = Document
         fields = '__all__'
+
+
+class AcceptanceStateSerializer(serializers.ModelSerializer):
+    document = serializers.PrimaryKeyRelatedField(queryset=Document.objects.all())
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+
+    class Meta:
+        model = AcceptanceState
+        fields = '__all__'
+
 
 class AttachmentSerializer(serializers.ModelSerializer):
     document = serializers.PrimaryKeyRelatedField(queryset=Document.objects.all())
