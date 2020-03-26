99cca82d235586b3f41446a10c50924e2494b265
frederic.everaert <frederic.everaert@crosslang.com>
2020-03-26 11:22:59 +0100

    django: register User to REST API

--- django/searchapp/admin.py
+++ django/searchapp/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+
 from admin_rest.models import site as rest_site
 
 from .models import Website, Attachment, Document, AcceptanceState
@@ -12,3 +14,5 @@ rest_site.register(Website)
 rest_site.register(Document)
 rest_site.register(Attachment)
 rest_site.register(AcceptanceState)
+
+rest_site.register(User)
