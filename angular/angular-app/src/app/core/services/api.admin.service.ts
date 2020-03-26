f0f708b85d04b2232c808115c5d375b90b51d234
frederic.everaert <frederic.everaert@crosslang.com>
2020-03-26 14:28:57 +0100

    angular: admin api service updated, begin admin mode for website detail

--- angular/angular-app/src/app/core/services/api.admin.service.ts
+++ angular/angular-app/src/app/core/services/api.admin.service.ts
@@ -2,8 +2,12 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Environment } from '../../../environments/environment-variables';
-import { map } from 'rxjs/operators';
+import { map, filter } from 'rxjs/operators';
 import { User, UserAdapter } from 'src/app/shared/models/user';
+import {
+  AcceptanceState,
+  AcceptanceStateAdapter
+} from 'src/app/shared/models/acceptanceState';
 
 @Injectable({
   providedIn: 'root'
@@ -11,13 +15,31 @@ import { User, UserAdapter } from 'src/app/shared/models/user';
 export class ApiAdminService {
   API_URL = Environment.ANGULAR_DJANGO_API_ADMIN_URL;
 
-  constructor(private http: HttpClient, private userAdapter: UserAdapter) {}
+  constructor(
+    private http: HttpClient,
+    private userAdapter: UserAdapter,
+    private stateAdapter: AcceptanceStateAdapter
+  ) {}
 
   public getUsers(): Observable<User[]> {
     return this.http
       .get<User[]>(`${this.API_URL}auth/user`)
       .pipe(
         map((items: any[]) => items.map(item => this.userAdapter.adapt(item)))
+      );
+  }
+
+  public getUser(id: number): Observable<User> {
+    return this.http
+      .get<User>(`${this.API_URL}auth/user/${id}`)
+      .pipe(map(item => this.userAdapter.adapt(item)));
+  }
+
+  public getStates(): Observable<AcceptanceState[]> {
+    return this.http
+      .get<AcceptanceState[]>(`${this.API_URL}searchapp/acceptancestate`)
+      .pipe(
+        map((items: any[]) => items.map(item => this.stateAdapter.adapt(item)))
       );
   }
 }
