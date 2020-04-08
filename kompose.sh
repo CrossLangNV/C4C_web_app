f86d791a35a1ed53a327dc7b846f62e2758b6bfa
Koen Van Winckel <koen.vanwinckel@crosslang.com>
2020-04-08 17:03:05 +0200

    i

--- kompose.sh
+++ kompose.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set +x # show commands
+set -x # show commands
 set -e # fail on first error
 VERSION=1.0.0
 HELM_USERNAME=crosslang
@@ -12,6 +12,7 @@ if [ -d "helm/fisma-ctlg-manager" ]; then
 fi
 mkdir -p helm
 cd helm
+echo $PWD
 docker run -v $PWD:/src -v $PWD/../docker-kompose.yml:/src/docker-compose.yaml -v $PWD/../secrets/django-docker.env:/src/secrets/django-docker.env --rm femtopixel/kompose convert -o fisma-ctlg-manager -c
 cd fisma-ctlg-manager
 docker run -v $PWD:/fisma-ctlg-manager -v $PWD:/apps --rm alpine/helm:latest package /fisma-ctlg-manager --version $VERSION
