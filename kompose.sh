a851fea6e74241443f321d97367fd828be3f20f6
Koen Van Winckel <koen.vanwinckel@crosslang.com>
2020-04-09 10:10:37 +0200

    added configmaps

--- kompose.sh
+++ kompose.sh
@@ -9,11 +9,16 @@ if [ ! -z "$BUILD_ID" ]; then
 fi
 if [ -d "helm" ]; then
   rm -R helm
+  echo "ok"
 fi
 mkdir helm
 cd helm
 echo $PWD
-docker run --user $(id -u):$(id -g) -v $PWD:/src -v $PWD/../docker-kompose.yml:/src/docker-compose.yaml --rm femtopixel/kompose convert -o fisma-ctlg-manager -c
+docker run --user $(id -u):$(id -g) \
+  -v $PWD:/src \
+  -v $PWD/../docker-kompose.yml:/src/docker-compose.yaml \
+  -v $PWD/../secrets:/src/secrets \
+  --rm femtopixel/kompose convert -o fisma-ctlg-manager -c
 cd fisma-ctlg-manager
 docker run --user $(id -u):$(id -g) -v $PWD:/fisma-ctlg-manager -v $PWD:/apps --rm alpine/helm:latest package /fisma-ctlg-manager --version $VERSION
 curl -u $HELM_USERNAME:$HELM_PASSWORD https://nexus.crosslang.com/repository/helm-repo/ --upload-file fisma-ctlg-manager-$VERSION.tgz -v
