4c15396c3bd8839e42e5933b334c1500f8b955b6
Koen Van Winckel <koen.vanwinckel@crosslang.com>
2020-04-09 11:29:33 +0200

    fix solr

--- kompose.sh
+++ kompose.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -x # show commands
+set +x # show commands
 set -e # fail on first error
 VERSION=1.0.0
 HELM_USERNAME=crosslang
@@ -9,18 +9,19 @@ if [ ! -z "$BUILD_ID" ]; then
 fi
 if [ -d "helm" ]; then
   rm -R helm
-  echo "ok"
 fi
 mkdir helm
 cd helm
 echo $PWD
 docker run --user $(id -u):$(id -g) \
   -v $PWD:/src \
-  -v $PWD/../docker-kompose.yml:/src/docker-compose.yaml \
+  -v $PWD/../docker-kompose.yaml:/src/docker-compose.yaml \
   -v $PWD/../secrets:/src/secrets \
   --rm femtopixel/kompose convert -o fisma-ctlg-manager -c
 cd fisma-ctlg-manager
-# work around https://github.com/kubernetes/kompose/issues/1096
+# work around: https://github.com/kubernetes/kompose/issues/1096
 patch -p0 templates/postgres-deployment.yaml ../../postgres-deployment.yaml.patch
+# work around: Cannot write to /var/solr as 8983:8983
+patch -p0 templates/solr-persistentvolumeclaim.yaml ../../solr-persistentvolumeclaim.yaml.patch
 docker run --user $(id -u):$(id -g) -v $PWD:/fisma-ctlg-manager -v $PWD:/apps --rm alpine/helm:latest package /fisma-ctlg-manager --version $VERSION
 curl -u $HELM_USERNAME:$HELM_PASSWORD https://nexus.crosslang.com/repository/helm-repo/ --upload-file fisma-ctlg-manager-$VERSION.tgz -v
