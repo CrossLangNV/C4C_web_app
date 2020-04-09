ba0e92542d8a0d61be98c931b7aad722ad629f1a
Koen Van Winckel <koen.vanwinckel@crosslang.com>
2020-04-09 14:30:50 +0200

    use env_file in angular app

---
+++ angular/angular-app/src/assets/env.js
@@ -0,0 +1,16 @@
+(function (window) {
+  window["env"] = window["env"] || {};
+
+  // Environment variables
+  window["env"]["ANGULAR_DJANGO_API_URL"] =
+    "http://localhost:8000/searchapp/api";
+  window["env"]["ANGULAR_DJANGO_API_ADMIN_URL"] =
+    "http://localhost:8000/admin/api";
+  window["env"]["ANGULAR_DJANGO_AUTH_URL"] = "http://localhost:8000/auth";
+  window["env"]["ANGULAR_GOOGLE_CLIENT_ID"] =
+    "540501250122-2k39tbh973rc7ufpl2nioor6muejkvok.apps.googleusercontent.com";
+  window["env"]["ANGULAR_DJANGO_CLIENT_ID"] =
+    "***REMOVED***";
+  window["env"]["ANGULAR_DJANGO_CLIENT_SECRET"] =
+    "***REMOVED***;
+})(this);
---
+++ angular/angular-app/src/assets/env.template.js
@@ -0,0 +1,13 @@
+(function (window) {
+  window["env"] = window["env"] || {};
+
+  // Environment variables
+  window["env"]["ANGULAR_DJANGO_API_URL"] = "${ANGULAR_DJANGO_API_URL}";
+  window["env"]["ANGULAR_DJANGO_API_ADMIN_URL"] =
+    "${ANGULAR_DJANGO_API_ADMIN_URL}";
+  window["env"]["ANGULAR_DJANGO_AUTH_URL"] = "${ANGULAR_DJANGO_AUTH_URL}";
+  window["env"]["ANGULAR_GOOGLE_CLIENT_ID"] = "${ANGULAR_GOOGLE_CLIENT_ID}";
+  window["env"]["ANGULAR_DJANGO_CLIENT_ID"] = "${ANGULAR_DJANGO_CLIENT_ID}";
+  window["env"]["ANGULAR_DJANGO_CLIENT_SECRET"] =
+    "${ANGULAR_DJANGO_CLIENT_SECRET}";
+})(this);
