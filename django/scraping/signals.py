7efc6f8e02d99ce2586bcf661bfbabca5255f9c2
Koen Van Winckel <koen.vanwinckel@crosslang.com>
2020-04-23 14:26:51 +0200

    Revert "merge"
    This reverts commit 389bc50b98a276800beb41e49f5ee0ceaf6a9f46.

---
+++ django/scraping/signals.py
@@ -0,0 +1,16 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from scraping.handlers import ScrapingTaskItemHandlerFactory
+from scraping.models import ScrapingTaskItem, ScrapingTask
+
+
+@receiver(post_save, sender=ScrapingTaskItem)
+def postprocess_scraping_item(sender, instance, created, **kwargs):
+    if created:
+        scraping_task = ScrapingTask.objects.get(pk=instance.task.id)
+        print("Saved scraping item from spider: " + scraping_task.spider)
+        # give ScrapingTaskItem instance to handler
+        factory = ScrapingTaskItemHandlerFactory(instance)
+        handler = factory.create_handler()
+        handler.process()
