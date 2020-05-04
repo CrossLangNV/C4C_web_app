903ceddc8f73c6dc89490a1a8df170ce9a98cafe
frederic.everaert <frederic.everaert@crosslang.com>
2020-05-04 13:54:22 +0200

    angular: deleted old, unused document-list component

--- angular/angular-app/src/app/dashboard/dashboard.module.ts
+++ angular/angular-app/src/app/dashboard/dashboard.module.ts
@@ -2,14 +2,10 @@ import { NgModule } from '@angular/core';
 import { UserListComponent } from './user-list/user-list.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { SharedModule } from '../shared/shared.module';
-import { DocumentListComponent } from './document-list/document-list.component';
 import { TableModule } from 'primeng/table';
-import { NgbdSortableHeaderDirective } from './document-list/document-list.component';
 @NgModule({
   declarations: [
     UserListComponent,
-    DocumentListComponent,
-    NgbdSortableHeaderDirective,
   ],
   imports: [SharedModule, DashboardRoutingModule, TableModule],
 })
