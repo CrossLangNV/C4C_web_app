aea2a78061d5200cc470112d46d46a98d99cee64
frederic.everaert <frederic.everaert@crosslang.com>
2020-05-11 15:33:25 +0200

    minio storage for export zip

--- django/scheduler/tasks.py
+++ django/scheduler/tasks.py
@@ -1,15 +1,18 @@
 from __future__ import absolute_import, unicode_literals
 
-import base64
 import logging
 import os
 import shutil
 
 from celery import shared_task
 from jsonlines import jsonlines
+from minio import Minio, ResponseError
+from minio.error import BucketAlreadyOwnedByYou, BucketAlreadyExists
+from scrapyd_api import ScrapydAPI
 
-from searchapp.models import Website
-from searchapp.solr_call import solr_search, solr_search_document_id_sorted
+from searchapp.datahandling import score_documents, sync_documents, sync_attachments
+from searchapp.models import Website, Document, Attachment
+from searchapp.solr_call import solr_search, solr_search_document_id_sorted, solr_search_website_sorted
 
 logger = logging.getLogger(__name__)
 workpath = os.path.dirname(os.path.abspath(__file__))
@@ -38,17 +41,22 @@ def export_documents(website_ids=None):
     zip_destination = workpath + '/export/' + export_documents.request.id
     shutil.make_archive(zip_destination, 'zip', workpath + '/export/jsonl')
 
+    # upload zip to minio
+    minio_client = Minio('minio:9000', access_key=os.environ['MINIO_ACCESS_KEY'],
+                         secret_key=os.environ['MINIO_SECRET_KEY'], secure=False)
+    try:
+        minio_client.make_bucket('export')
+    except BucketAlreadyOwnedByYou as err:
+        pass
+    except BucketAlreadyExists as err:
+        pass
+    except ResponseError as err:
+        raise
+    minio_client.fput_object('export', export_documents.request.id + '.zip', zip_destination + '.zip')
+
 
 @shared_task
-def export_get_zip(task_id):
-    file = open(workpath + '/export/' + task_id + '.zip', 'rb')
-    bytes = file.read()
-    base64_bytes = base64.b64encode(bytes)
-    return base64_bytes.decode('ascii')
-
-
-@shared_task
-def export_delete_jsonl():
+def export_delete(task_id):
     # delete export jsonl contents
     for filename in os.listdir(workpath + '/export/jsonl'):
         file_path = os.path.join(workpath + '/export/jsonl', filename)
@@ -60,9 +68,66 @@ def export_delete_jsonl():
         except Exception as e:
             logger.error('Failed to delete %s. Reason: %s' %
                          (file_path, e))
+    # delete zip with given task id
+    os.remove(workpath + '/export/' + task_id + '.zip')
 
 
 @shared_task
-def export_delete_zip(task_id):
-    # delete zip with given task id
-    os.remove(workpath + '/export/' + task_id + '.zip')
+def score_documents_task(website_id):
+    logger.info("Scoring documents with WEBSITE: " + str(website_id))
+    # lookup documents for website and score them
+    website = Website.objects.get(pk=website_id)
+    django_documents = Document.objects.filter(website=website).order_by('id')
+    score_documents(django_documents)
+
+
+@shared_task
+def sync_documents_task(website_id):
+    logger.info("Syncing documents with WEBSITE: " + str(website_id))
+    # lookup documents for website and sync them
+    website = Website.objects.get(pk=website_id)
+    django_documents = Document.objects.filter(website=website).order_by('id')
+    # query Solr for available documents and sync with Django
+    solr_documents = solr_search_website_sorted(
+        core='documents', website=website.name.lower())
+    sync_documents(website, solr_documents, django_documents)
+
+
+@shared_task
+def sync_attachments_task(website_id):
+    logger.info("Synching attachments with WEBSITE: " + str(website_id))
+    # lookup documents for website and sync them
+    website = Website.objects.get(pk=website_id)
+    for document in Document.objects.filter(website=website).order_by('id'):
+        # query Solr for attachments
+        solr_files = solr_search_document_id_sorted(
+            core='files', document_id=str(document.id))
+        attachments = Attachment.objects.filter(
+            document=document).order_by('id')
+        sync_attachments(document, solr_files, attachments)
+
+
+@shared_task
+def scrape_website_task(website_id):
+    logger.info("Scraping with WEBSITE: " + str(website_id))
+    # lookup website and start scraping
+    website = Website.objects.get(pk=website_id)
+    # custom settings for spider
+    settings = {
+        'task_id': '1'
+    }
+    spiders = [{"id": "bis"}, {"id": "eiopa"}, {"id": "esma"}, {
+        "id": "eurlex", "type": "directives"}, {"id": "eurlex", "type": "decisions"},
+               {"id": "eurlex", "type": "regulations"}, {"id": "fsb"}, {"id": "srb"},
+               {"id": "eba", "type": "guidelines"}, {
+                   "id": "eba", "type": "recommendations"},
+               ]
+    for spider in spiders:
+        if spider['id'].lower() == website.name.lower():
+            scrapyd = ScrapydAPI(os.environ['SCRAPYD_URL'])
+            scrapyd_project = 'default'
+            if 'type' not in spider:
+                spider['type'] = ''
+            # schedule scraping task
+            scrapyd_task_id = scrapyd.schedule(
+                'default', spider['id'], settings=settings, spider_type=spider['type'])
