00d2d740cff2deafc3c302b5b0949090d2f660ad
frederic.everaert <frederic.everaert@crosslang.com>
2020-05-25 13:17:54 +0200

    django: concept many-to-many for documents

--- django/glossary/models.py
+++ django/glossary/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 
+from searchapp.models import Document
+
 
 class Concept(models.Model):
     name = models.CharField(max_length=200, unique=True)
     definition = models.TextField()
+    documents = models.ManyToManyField(Document)
+
+    class Meta:
+        ordering = ['name']
 
     def __str__(self):
         return self.name
