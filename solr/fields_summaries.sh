fcec657cc32fce5e919cded65f41d589faf42123
frederic.everaert <frederic.everaert@crosslang.com>
2020-05-27 16:49:15 +0200

    solr: fields for summaries core

---
+++ solr/fields_summaries.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+COLLECTION="summaries"
+SOLR_HOST="http://localhost:8983"
+
+JSON='{"add-field": [
+{"name":"title",            "type":"text_general","stored":true,"indexed":true,"multiValued":true},
+{"name":"url"               "type":"text_general","stored":true,"indexed":true,"multiValued":true},
+{"name":"celex",            "type":"text_general","stored":true,"indexed":true,"multiValued":true},
+{"name":"html_content",     "type":"text_general","stored":true,"indexed":true,"multiValued":true},
+{"name":"pull",             "type":"boolean","stored":true,"indexed":true,"multiValued":true},
+
+{"name":"classifications",          "type":"text_general","stored":true,"indexed":true,"multiValued":true},
+{"name":"classifications_code",     "type":"text_general","stored":true,"indexed":true,"multiValued":true},
+{"name":"classifications_label",    "type":"text_general","stored":true,"indexed":true,"multiValued":true},
+{"name":"classifications_type",     "type":"text_general","stored":true,"indexed":true,"multiValued":true},
+
+{"name":"dates",                    "type":"pdates","stored":true,"indexed":true,"multiValued":true},
+{"name":"dates_info",               "type":"text_general","stored":true,"indexed":true,"multiValued":true},
+{"name":"dates_type",               "type":"text_general","stored":true,"indexed":true,"multiValued":true},
+
+{"name":"created_at",               "type":"pdates","stored":true,"indexed":true,"multiValued":true},
+{"name":"updated_at",               "type":"pdates","stored":true,"indexed":true,"multiValued":true},
+
+{"name":"spider",          "type":"text_general","stored":true,"indexed":true,"multiValued":true},
+{"name":"task",            "type":"plongs","stored":true,"indexed":true,"multiValued":true},
+]
+}';
+
+JSON_DYNAMIC_FIELDS='{"add-dynamic-field": [
+{"name":"misc_*", "type":"text_general", "stored":true, "indexed":true, "multiValued":true},
+]
+}'
+
+curl -k -X POST -H 'Content-type:application/json' --data-binary "$JSON" $SOLR_HOST/solr/$COLLECTION/schema
+curl -k -X POST -H 'Content-type:application/json' --data-binary "$JSON_DYNAMIC_FIELDS" $SOLR_HOST/solr/$COLLECTION/schema
