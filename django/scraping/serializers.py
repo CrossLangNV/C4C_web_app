1c7735a38300620252bb3f191c18c6cd3855fadd
frederic.everaert <frederic.everaert@crosslang.com>
2020-06-02 14:04:14 +0200

    django: scraping task can launch multiple items

--- django/scraping/serializers.py
+++ django/scraping/serializers.py
@@ -1,10 +1,18 @@
 from rest_framework import serializers
 
-from .models import ScrapingTask
+from .models import ScrapingTask, ScrapingTaskItem
+
+
+class ScrapingTaskItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ScrapingTaskItem
+        fields = ['id', 'scheduler_id', 'status', 'date']
 
 
 class ScrapingTaskSerializer(serializers.ModelSerializer):
+    items = ScrapingTaskItemSerializer(many=True, read_only=True)
+
     class Meta:
         model = ScrapingTask
-        fields = ['id', 'scheduler_id', 'spider',
-                  'spider_type', 'status', 'date']
+        fields = ['id', 'spider', 'spider_date_start', 'spider_date_end',
+                  'spider_type', 'date', 'items']
