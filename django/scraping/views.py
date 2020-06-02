f1b8d79aac8b3b87b9b4d8e4bb2526b3dbedd727
frederic.everaert <frederic.everaert@crosslang.com>
2020-06-02 14:04:14 +0200

    django: scraping task can launch multiple items

--- django/scraping/views.py
+++ django/scraping/views.py
@@ -1,6 +1,8 @@
 import os
 
 import logging
+from datetime import datetime
+
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.utils.decorators import method_decorator
@@ -12,7 +14,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from scrapyd_api import ScrapydAPI
 
-from .models import ScrapingTask
+from .models import ScrapingTask, ScrapingTaskItem
 from .serializers import ScrapingTaskSerializer
 
 logger = logging.getLogger(__name__)
@@ -29,11 +31,13 @@ class ScrapingTemplateView(View, ContextMixin, TemplateResponseMixin):
         scraped_tasks = ScrapingTask.objects.all().order_by("-date")
         # FIXME: get list from http://localhost:6800/listspiders.json?project=default ?
         spiders = [{"id": "bis"}, {"id": "eiopa"}, {"id": "esma"}, {
-            "id": "eurlex", "type": "directives"}, {"id": "eurlex", "type": "decisions"}, {"id": "eurlex", "type": "regulations"}, {"id": "fsb"}, {"id": "srb"},
-            {"id": "eba", "type": "guidelines"}, {
-                "id": "eba", "type": "recommendations"},
-        ]
-        return render(request, self.template_name, {'scraped_tasks': scraped_tasks, 'nav': 'scraping', 'spiders': spiders})
+            "id": "eurlex", "type": "directives"}, {"id": "eurlex", "type": "decisions"},
+                   {"id": "eurlex", "type": "regulations"}, {"id": "fsb"}, {"id": "srb"},
+                   {"id": "eba", "type": "guidelines"}, {
+                       "id": "eba", "type": "recommendations"},
+                   ]
+        return render(request, self.template_name,
+                      {'scraped_tasks': scraped_tasks, 'nav': 'scraping', 'spiders': spiders})
 
     # new scraping task
     def post(self, request, spider):
@@ -75,16 +79,16 @@ class ScrapingTaskListView(ListCreateAPIView):
     scrapyd_project = 'default'
 
     def get(self, request, *args, **kwargs):
-        # get tasks + status per task
+        # get tasks + status per task item
         queryset = self.get_queryset()
         serializer_read = ScrapingTaskSerializer(queryset, many=True)
-        for task_data in serializer_read.data:
-            if task_data['scheduler_id']:
-                status = self.scrapyd.job_status(
-                    self.scrapyd_project, task_data['scheduler_id'])
-                task = ScrapingTask.objects.get(pk=task_data['id'])
-                task.status = status
-                task.save()
+        tasks = serializer_read.data
+        for task in tasks:
+            for item_data in task['items']:
+                status = self.scrapyd.job_status(self.scrapyd_project, item_data['scheduler_id'])
+                item = ScrapingTaskItem.objects.get(pk=item_data['id'])
+                item.status = status
+                item.save()
         return Response(serializer_read.data)
 
     def post(self, request, *args, **kwargs):
@@ -96,11 +100,29 @@ class ScrapingTaskListView(ListCreateAPIView):
             settings = {
                 'task_id': scraping_task.id
             }
-            scheduler_id = self.scrapyd.schedule(self.scrapyd_project, scraping_task.spider, settings=settings,
-                                                 spider_type=scraping_task.spider_type)
-            scraping_task.scheduler_id = scheduler_id
-            scraping_task.save()
-            return Response(serializer.data, status=201)
+            if scraping_task.spider_date_start and scraping_task.spider_date_end:
+                date_start = scraping_task.spider_date_start.strftime('%d%m%Y')
+                date_end = scraping_task.spider_date_end.strftime('%d%m%Y')
+                scheduler_id = self.scrapyd.schedule(self.scrapyd_project, scraping_task.spider, settings=settings,
+                                                     spider_type=scraping_task.spider_type,
+                                                     spider_date_start=date_start,
+                                                     spider_date_end=date_end)
+                ScrapingTaskItem.objects.create(scheduler_id=scheduler_id, task=scraping_task)
+
+            else:
+                # launch spider per 10 years
+                year_stop = 1959
+                today = datetime.today()
+                for year in range(today.year, year_stop, -10):
+                    start = '0101' + str(year - 10)
+                    end = '3112' + str(year)
+                    scheduler_id = self.scrapyd.schedule(self.scrapyd_project, scraping_task.spider, settings=settings,
+                                                         spider_type=scraping_task.spider_type,
+                                                         spider_date_start=start,
+                                                         spider_date_end=end)
+                    ScrapingTaskItem.objects.create(scheduler_id=scheduler_id, task=scraping_task)
+                return Response(serializer.data, status=201)
+
         return Response(serializer.errors, status=400)
 
 
@@ -115,11 +137,10 @@ class ScrapingTaskView(RetrieveDestroyAPIView):
         queryset = self.get_queryset()
         task_qs = queryset.filter(pk=self.kwargs['pk'])
         task = task_qs[0]
-        if task.scheduler_id:
-            status = self.scrapyd.job_status(
-                self.scrapyd_project, task.scheduler_id)
-            task.status = status
-            task.save()
+        for item in task.items.all():
+            status = self.scrapyd.job_status(self.scrapyd_project, item.scheduler_id)
+            item.status = status
+            item.save()
         return task
 
 
