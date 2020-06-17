5f0c11f793fe8b23cf525e4eecd5b781a55d4047
frederic.everaert <frederic.everaert@crosslang.com>
2020-06-17 16:40:12 +0200

    angular: hack to hide 'means' highlighting in defined by docs

--- angular/angular-app/src/app/glossary/concept-detail/concept-detail.component.ts
+++ angular/angular-app/src/app/glossary/concept-detail/concept-detail.component.ts
@@ -124,7 +124,9 @@ export class ConceptDetailComponent implements OnInit {
         const solrDocumentIds = solrDocuments.map((solrDoc) => solrDoc.id);
         this.getDocuments(solrDocumentIds).subscribe((doc) => {
           doc.forEach((document, index) => {
-            document.content = solrDocuments[index].content;
+            let solrContent = solrDocuments[index].content;
+            solrContent = solrContent.map(text => text.replace('<span class=\"highlight\">means</span>', 'means'));
+            document.content = solrContent;
             this.definedIn.push(document);
           });
         });
