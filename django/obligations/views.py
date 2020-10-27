8621d69c968ca77421a563f71e3ce5585f568888
Oan Stultjens <oan.stultjens@crosslang.com>
2020-10-27 16:34:50 +0100

    Added models for Reporting Obligations

---
+++ django/obligations/views.py
@@ -0,0 +1,28 @@
+from obligations.models import ReportingObligation
+from obligations.serializers import ReportingObligationSerializer
+from rest_framework import filters
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.pagination import PageNumberPagination
+
+
+class SmallResultsSetPagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = 'page_size'
+    max_page_size = 1000
+
+
+class ReportingObligationListAPIView(ListCreateAPIView):
+    # permission_classes = [permissions.IsAuthenticated]
+    pagination_class = SmallResultsSetPagination
+    queryset = ReportingObligation.objects.all()
+    serializer_class = ReportingObligationSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['name']
+
+    def get_queryset(self):
+        q = ReportingObligation.objects.all()
+        keyword = self.request.GET.get('keyword', "")
+        if keyword:
+            q = q.filter(name__icontains=keyword)
+
+        return q.order_by("name")
