1847bae795ff5136c14a6a4d398ec12996dd9d62
Oan Stultjens <oan.stultjens@crosslang.com>
2020-10-28 10:30:40 +0100

    - Reporting obligations loaded in Angular from Django - RO's back in navigation bar

--- django/obligations/admin.py
+++ django/obligations/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from obligations.models import ReportingObligation
+
+
+class ReportingObligationAdmin(admin.ModelAdmin):
+    actions = ['delete_all_reporting_obligations']
+
+    def delete_all_reporting_obligations(self, request, queryset):
+        ReportingObligation.objects.all().delete()
+
+
+admin.site.register(ReportingObligation, ReportingObligationAdmin)
