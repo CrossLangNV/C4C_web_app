55c16ad531e52a58832a34b235bd0859a5da75ec
Oan Stultjens <oan.stultjens@crosslang.com>
2020-11-04 11:38:21 +0100

    Implementing RO's API for external Jena/Fuseki

--- django/obligations/rdf_call.py
+++ django/obligations/rdf_call.py
@@ -4,80 +4,37 @@ import logging as logger
 import os
 
 from obligations.models import ReportingObligation
-
-
-def rdf_search(dataset="", subject=None, predicate=None, obj=None):
-    if subject is None:
-        _subject = '?s'
-    else:
-        _subject = f'<{subject}>'
-
-    if predicate is None:
-        _predicate = '?s'
-    else:
-        _predicate = f'<{predicate}>'
-
-    if obj is None:
-        _obj = '?s'
-    else:
-        _obj = f'<{obj}>'
-
-    # same for pred and obj
-    sparql = SPARQLWrapper(os.environ['RDF_URL']+"/"+dataset)
-    q = f"""SELECT {_subject} {_predicate} {_obj}
-        WHERE {{
-           {_subject} {_predicate} {_obj}
-        }}
-        LIMIT 25"""
-    sparql.setQuery(q)
-    sparql.setReturnFormat(JSON)
-    results = sparql.query().convert()
-    logger.info(results)
-    return results
+from obligations import build_rdf
+from obligations.build_rdf import D_ENTITIES, ExampleCasContent, ROGraph
+from obligations.rdf_parser import SPARQLReportingObligationProvider, RDFLibGraphWrapper, SPARQLGraphWrapper
+from obligations.rdf_mock import rdf_get_reporters_mock, rdf_get_verbs_mock, rdf_get_reports_mock,\
+    rdf_get_regulatory_body_mock, rdf_get_propmod_mock, rdf_get_entity_mock, rdf_get_frequency_mock
 
 
 def rdf_get_reporters():
-    # TODO Mock
-    reporters = ReportingObligation.objects.all()
-    result = []
-
-    for reporter in reporters:
-        result.append(reporter.name)
-
-    return result
+    return rdf_get_reporters_mock()
 
 
 def rdf_get_verbs():
-    # TODO Mock
-    verbs = ["shall", "must"]
-    return verbs
+    return rdf_get_verbs_mock()
 
 
 def rdf_get_reports():
-    # TODO Mock
-    reports = ["report", "submit", "reviewed", "review"]
-    return reports
+    return rdf_get_reports_mock()
 
 
 def rdf_get_regulatory_body():
-    # TODO Mock
-    regulatory_body = ["to the Commission", "to the competent authorities", "to the board", "to the council", "to the president", "to the management"]
-    return regulatory_body
+    return rdf_get_regulatory_body_mock()
 
 
 def rdf_get_propmod():
-    # TODO Mock
-    prop_mod = ["in accordance with the reporting requirements set out in Article 415(1 ) and the uniform reporting formats referred to in Article 415(3 )", "in accordance with requirements"]
-    return prop_mod
+    return rdf_get_propmod_mock()
 
 
 def rdf_get_entity():
-    # TODO Mock
-    entity = ["those draft regulatory technical standards", "inflows from any of the liquid assets reported in accordance with Article 416 other than payments due on the assets that are not reflected in the market value of the asset", "inflows from any new obligations entered into", "about the result of the process referred to in Article 20(1)(b )", "Fulfilment of the conditions for such higher inflows"]
-    return entity
+    return rdf_get_entity_mock()
 
 
 def rdf_get_frequency():
-    # TODO Mock
-    frequency = ["regularly", "by 1 January 2015", "daily", "monthly", "yearly", "upon request", "within a deadline set by the board", "in the previous financial year"]
-    return frequency
+    return rdf_get_frequency_mock()
+
---
+++ django/obligations/rdf_parser.py
@@ -0,0 +1,213 @@
+import abc
+import os
+from typing import Iterable, List, Tuple
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+import rdflib
+
+from obligations import build_rdf
+
+ROOT = os.path.join(os.path.dirname(__file__), '..')
+
+
+class GraphWrapper(abc.ABC):
+    """
+    Abstract method for accessing RDF's for querying.
+    """
+
+    @abc.abstractmethod
+    def query(self, q: str) -> Iterable[tuple]:
+        """
+        Args:
+            q: SPARQL query string
+        Returns:
+            Iterable with tuples of query results.
+        """
+        pass
+
+
+class RDFLibGraphWrapper(GraphWrapper):
+    def __init__(self, path_rdf):
+        super(RDFLibGraphWrapper, self).__init__()
+        g = rdflib.Graph()
+        g.parse(path_rdf)
+
+        self.g = g
+
+    def query(self, q):
+        qres = self.g.query(q)
+
+        # TODO use qres.bindings
+
+        return qres
+
+
+class SPARQLGraphWrapper(GraphWrapper):
+    def __init__(self, endpoint):
+        self.sparql = SPARQLWrapper(endpoint)
+        self.sparql.setReturnFormat(JSON)
+
+        # TODO connect to endpoint to allow querying
+
+    def query(self, q: str) -> Iterable[tuple]:
+        self.sparql.setQuery(q)
+        ret = self.sparql.query()
+        results = ret.convert()
+
+        l = []
+        for result in results["results"]["bindings"]:
+            list(result.keys())
+
+            l_i = tuple(result[k]['value'] for k in list(result.keys()))
+
+            l.append(l_i)
+
+        return l
+
+
+class SPARQLReportingObligationProvider:
+    def __init__(self, graph_wrapper: GraphWrapper):
+        self.graph_wrapper = graph_wrapper
+
+    def get_different_entities(self):
+        q = """
+            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+            PREFIX dgfro: <http://dgfisma.com/reporting_obligation#>
+            SELECT DISTINCT ?pred ?entClass
+            WHERE {
+                ?pred rdfs:domain dgfro:ReportingObligation .
+                ?pred rdfs:range ?entClass .
+                ?_ro 			rdf:type dgfro:ReportingObligation .
+
+            FILTER ( EXISTS { ?entClass rdfs:subClassOf skos:Concept . } ||
+                ?entClass = skos:Concept 
+            )
+            }
+        """
+
+        l = list(self.graph_wrapper.query(q))
+
+        l_entity_predicates = [str(a) for a, *_ in l]
+
+        return l_entity_predicates
+
+    def get_all_from_type(self, type_uri,
+                          distinct=False) -> List[str]:
+        q = f"""
+            PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+            PREFIX dgfro: <http://dgfisma.com/reporting_obligation#>
+            SELECT {'DISTINCT' if distinct else ''} ?value
+            WHERE {{
+                ?ro <{type_uri}> ?ent .
+    			?ent skos:prefLabel ?value
+                FILTER (lang(?value) = 'en')
+            }}
+        """
+        l = list(self.graph_wrapper.query(q))
+
+        l_values = [str(a) for a, *_ in l]
+
+        return l_values
+
+    def get_all_ro_uri(self) -> List[str]:
+        q = f"""
+            PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+            PREFIX dgfro: <http://dgfisma.com/reporting_obligation#>
+            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            SELECT ?ro_id
+            WHERE {{
+                ?ro_id rdf:type <{build_rdf.ROGraph.class_rep_obl}> .
+            }}
+            """
+        l = self.graph_wrapper.query(q)
+
+        l_uri = [str(a) for a, *_ in l]
+
+        return l_uri
+
+    def get_all_ro_str(self):
+        q = f"""
+            PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+            PREFIX dgfro: <http://dgfisma.com/reporting_obligation#>
+            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            SELECT ?value ?ro_id
+            WHERE {{
+                ?ro_id rdf:type <{build_rdf.ROGraph.class_rep_obl}> ;
+                    rdf:value ?value
+            }}
+        """
+        l = self.graph_wrapper.query(q)
+
+        l_ro = [str(a) for a, *_ in l]
+
+        return l_ro
+
+    def get_filter_single(self, pred, value) -> List[str]:
+        """ Retrieve reporting obligations with a matching value for certain predicate
+        Args:
+            pred:
+            value:
+        Returns:
+        """
+
+        q = f"""
+            PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+            PREFIX dgfro: <http://dgfisma.com/reporting_obligation#>
+            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            SELECT ?value ?ro_id ?p
+            WHERE {{
+                ?ro_id rdf:type <{build_rdf.ROGraph.class_rep_obl}> ;
+                    rdf:value ?value ;
+                    <{pred}> ?ent .
+                ?ent skos:prefLabel ?p
+                FILTER (lang(?p) = "en"   )
+                FILTER ( str(?p) ="{value}"    )
+            }}
+        """
+        l = self.graph_wrapper.query(q)
+
+        l_ro = [str(a) for a, *_ in l]
+
+        return l_ro
+
+    def get_filter_multiple(self, list_pred_value: List[Tuple[str]] = []) -> List[str]:
+        """ Retrieve reporting obligations with a matching value for certain predicate
+        Args:
+            list_pred_value: List[(pred:str, value:str)]
+            e.g. [ ("<pred 1>", "<value 1>"),
+                    ...
+                    ("<pred n>", "<value n>") ]
+        Returns:
+        """
+
+        q = f"""
+            PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+            PREFIX dgfro: <http://dgfisma.com/reporting_obligation#>
+            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            SELECT ?value ?ro_id ?p
+            WHERE {{
+                ?ro_id rdf:type <{build_rdf.ROGraph.class_rep_obl}> ;
+                   rdf:value ?value .
+            """
+
+        for i, (pred, value) in enumerate(list_pred_value):
+            q_i = f"""
+                     ?ro_id  <{pred}> ?ent{i} .
+                        ?ent{i} skos:prefLabel ?p{i} .
+                                FILTER (lang(?p{i}) = "en"   )
+                                FILTER ( str(?p{i}) ="{value}"    )
+            """
+
+            q += q_i
+
+        q += f"""
+        }}
+        """
+
+        l = self.graph_wrapper.query(q)
+
+        l_ro = [str(a) for a, *_ in l]
+
+        return l_ro
