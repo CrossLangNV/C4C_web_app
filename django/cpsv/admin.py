d2119af574a5b88031ea58bb1ab1623fdf1595e7
Oan Stultjens <oan.stultjens@crosslang.com>
2021-01-27 11:24:52 +0100

    Updated models (PublicService, ContactPoint)

--- django/cpsv/admin.py
+++ django/cpsv/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from cpsv.models import PublicService, PublicServiceConcept, ContactPoint
+
+admin.site.register(PublicService)
+admin.site.register(PublicServiceConcept)
+admin.site.register(ContactPoint)
--- django/cpsv/models.py
+++ django/cpsv/models.py
@@ -1,39 +1,38 @@
-from time import timezone
 
 from django.db import models
+from django.utils import timezone
 
 from glossary.models import Concept
 
 
-class CpsvBase(models.Model):
-    title = models.TextField(unique=True)
-    url = models.CharField(max_length=200)
+class PublicService(models.Model):
+    name = models.TextField(unique=True)
+    description = models.TextField()
+    identifier = models.CharField(max_length=200)
+
+    concepts = models.ManyToManyField(
+        Concept,
+        through='PublicServiceConcept',
+        through_fields=('public_service', 'concept'),
+        related_name='public_service_concept'
+    )
 
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        abstract = True
-        ordering = ['title']
+        ordering = ['name']
 
     def __str__(self):
-        return self.title
+        return self.name
 
 
-class PublicService(CpsvBase):
-    class Meta(CpsvBase.Meta):
-        pass
+class PublicServiceConcept(models.Model):
+    public_service = models.ForeignKey(PublicService, on_delete=models.CASCADE)
+    concept = models.ForeignKey(Concept, on_delete=models.CASCADE)
 
 
-class ContactPoint(CpsvBase):
-    class Meta(CpsvBase.Meta):
-        pass
+class ContactPoint(models.Model):
+    identifier = models.CharField(max_length=200)
+    opening_hours = models.TextField()
 
-
-class LiveEvent(CpsvBase):
-    class Meta(CpsvBase.Meta):
-        pass
-
-
-class Phone(models.Model):
-    value = models.CharField(max_length=200, db_index=True)
